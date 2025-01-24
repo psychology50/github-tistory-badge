3b63a7e912a764f35e51fd276aac7a1b0e704cd1
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-01-24 20:37:58 +0900

    edit readme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1>Github Tistory Badge</h1>
       <p>Add to your GitHub profile:</p>
       <code>
-        ![Blog](https://github-tistory-badge-g7mtf8e1f-jayangs-projects-f6d216ab.vercel.app/api/badge?name=jaeseo0519)
+        ![Tistory](https://github-tistory-badge.vercel.app/api/badge?name=jaeseo0519)
       </code>
     </div>
   );
